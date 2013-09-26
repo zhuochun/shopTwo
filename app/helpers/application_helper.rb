518126955539405269f5b9e10b83354ee3b6af4d
Wang Zhuochun <stone1551@gmail.com>
2013-09-26 19:07:38 +0800

    add different icons for user roles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # Public: create an icon for user
+  #
+  # Returns an html icon
+  def user_icon(user)
+    if user.role == "administrator" || user.role == "manager"
+      "<i class='glyphicon glyphicon-tower'></i>".html_safe
+    else
+      "<i class='glyphicon glyphicon-user'></i>".html_safe
+    end
+  end
+
 end
