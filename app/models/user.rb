353b852bd8e49edc5f5bfddd485fc96b005be4af
Wang Zhuochun <stone1551@gmail.com>
2013-10-01 14:50:10 +0800

    modify account edit page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Default roles
   ROLES = %w(customer employee manager administrator)
+  # For each role
+  CUSTOMER, EMPLOYEE, MANAGER, ADMIN = ROLES
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
