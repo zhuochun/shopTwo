a4f25d442074bd31926a3ec7100b1279a96f8039
Wang Zhuochun <stone1551@gmail.com>
2013-10-22 21:39:53 +0800

    modifications on htmls

--- app/models/settle_item.rb
+++ app/models/settle_item.rb
@@ -1,4 +1,12 @@
 class SettleItem < ActiveRecord::Base
+  # relationships
   belongs_to :settlement
   belongs_to :product, foreign_key: 'barcode'
+
+  # hooks
+  before_create :deduct_stock_from_store
+  def deduct_stock_from_store
+    # TODO
+  end
+
 end
