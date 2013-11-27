b61734d176393d32048f9cfc5428733e0ac43637
Wang Zhuochun <stone1551@gmail.com>
2013-11-28 04:57:00 +0800

    fix line item destroy typo

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -58,7 +58,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_item.cart }
+      format.html { redirect_to @line_item.cart }
       format.json { head :no_content }
     end
   end
