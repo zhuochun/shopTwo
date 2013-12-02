0130b642d2135330e8ab52e289421b6926a96348
Wang Zhuochun <stone1551@gmail.com>
2013-12-02 22:24:45 +0800

    add all products page view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.paginate(page: params[:page], per_page: 50)
+    @products = Product.paginate(page: params[:page], per_page: 40)
   end
 
   # GET /products/1
